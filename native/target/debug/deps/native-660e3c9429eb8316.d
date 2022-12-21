37171eff5fe38a24bcd20188ff8ecfe2677ea4b9
Kelvin <quocvm1@gmail.com>
2022-12-21 16:39:20 +0800

    try to call rust from flutter, FAILED

--- native/target/debug/deps/native-660e3c9429eb8316.d
+++ native/target/debug/deps/native-660e3c9429eb8316.d
@@ -1,5 +1,6 @@
-/Users/kelvin/OpenSource/ev_diag/native/target/debug/deps/native-660e3c9429eb8316.rmeta: src/lib.rs
+/Users/kelvin/OpenSource/ev_diag/native/target/debug/deps/native-660e3c9429eb8316.rmeta: src/lib.rs src/api.rs
 
-/Users/kelvin/OpenSource/ev_diag/native/target/debug/deps/native-660e3c9429eb8316.d: src/lib.rs
+/Users/kelvin/OpenSource/ev_diag/native/target/debug/deps/native-660e3c9429eb8316.d: src/lib.rs src/api.rs
 
 src/lib.rs:
+src/api.rs:
--- native/target/debug/deps/native-bc9d04afdbc2ad3d.d
+++ native/target/debug/deps/native-bc9d04afdbc2ad3d.d
@@ -1,5 +1,6 @@
-/Users/kelvin/OpenSource/ev_diag/native/target/debug/deps/native-bc9d04afdbc2ad3d.rmeta: src/lib.rs
+/Users/kelvin/OpenSource/ev_diag/native/target/debug/deps/native-bc9d04afdbc2ad3d.rmeta: src/lib.rs src/api.rs
 
-/Users/kelvin/OpenSource/ev_diag/native/target/debug/deps/native-bc9d04afdbc2ad3d.d: src/lib.rs
+/Users/kelvin/OpenSource/ev_diag/native/target/debug/deps/native-bc9d04afdbc2ad3d.d: src/lib.rs src/api.rs
 
 src/lib.rs:
+src/api.rs:
